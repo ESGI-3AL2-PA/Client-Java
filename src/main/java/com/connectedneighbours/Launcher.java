8b7d4984d3edf721d4af9cf79ba8cbfc308d0be6
Tripticon84 <yannjou@free.fr>
2026-04-05 16:23:15 +0200

    Add console mode option to Launcher

--- src/main/java/com/connectedneighbours/Launcher.java
+++ src/main/java/com/connectedneighbours/Launcher.java
@@ -1,8 +1,13 @@
 package com.connectedneighbours;
 
+import java.sql.SQLException;
 
 public class Launcher {
-    public static void main(String[] args) {
-        MainApp.main(args);
+    public static void main(String[] args) throws SQLException {
+        if (args.length > 0 && args[0].equals("--console")) {
+            new ConsoleApp().run();
+        } else {
+            MainApp.main(args);
+        }
     }
 }
